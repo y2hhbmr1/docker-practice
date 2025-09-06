f4238a8610ef2300a75d5bfe755b56328a4dce72
y2hhbmr1 <y2hhbmr1+glsl@bugcrowdninja.com>
2025-09-06 21:13:34 +0530

    44. Save and Return Users in Memory

--- containerize-express-app/index.js
+++ containerize-express-app/index.js
@@ -3,16 +3,34 @@ const bodyParser = require("body-parser");
 
 const app = express();
 const port = 3000;
+const users = [];
 
-// Middleware to parse JSON request bodies
 app.use(bodyParser.json());
 
-// Route: GET /
 app.get("/", (req, res) => {
-  res.send("Hello World");
+  res.send("Hello world!");
 });
 
-// Start the server
+// Get registered users
+app.get("/users", (req, res) => {
+  return res.json({ users });
+});
+
+// Register a new user
+app.post("/users", (req, res) => {
+  const newUserId = req.body.userId;
+  if (!newUserId) {
+    return res.status(400).send("Missing userId.");
+  }
+
+  if (users.includes(newUserId)) {
+    return res.status(400).send("userId already exists.");
+  }
+
+  users.push(newUserId);
+  return res.status(201).send("User registered.");
+});
+
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
 });
