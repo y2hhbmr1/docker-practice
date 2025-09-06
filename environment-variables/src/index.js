fcfa3b3b2f3c655716771d6e31b850ae6083f79e
y2hhbmr1 <y2hhbmr1+glsl@bugcrowdninja.com>
2025-09-07 02:11:36 +0530

    52. Hands-On: Introduction to Environment Variables

---
+++ environment-variables/src/index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+
+const app = express();
+const port = process.env.PORT;
+
+app.get("/", (req, res) => {
+  res.send("Hello from express");
+});
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
