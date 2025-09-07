2a4ca56927a08fb87c89ec7ea7dfd857ebe66e4c
y2hhbmr1 <y2hhbmr1+glsl@bugcrowdninja.com>
2025-09-07 19:35:43 +0530

    57. Hands-On: Motivation to Multistage Dockerfiles

---
+++ multistage-builds/src/index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello from Express!");
+});
+
+const port = process.env.PORT;
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
