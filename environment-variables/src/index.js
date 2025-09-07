99aec7ef6e219269915d0b31f8a712747323ecc1
y2hhbmr1 <y2hhbmr1+glsl@bugcrowdninja.com>
2025-09-07 15:27:52 +0530

    53. Hands-On: Setting Environment Variables via the CLI

--- environment-variables/src/index.js
+++ environment-variables/src/index.js
@@ -4,7 +4,7 @@ const app = express();
 const port = process.env.PORT;
 
 app.get("/", (req, res) => {
-  res.send("Hello from express");
+  res.send(`Hello from ${process.env.APP_NAME}`);
 });
 
 app.listen(port, () => {
