bbf3d8d74797abae2513e3db2f9030f8ba5e1a34
y2hhbmr1 <y2hhbmr1+glsl@bugcrowdninja.com>
2025-09-08 21:00:51 +0530

    90. Defining Ports, Volumes, and Networks

--- key-value-app/start-db.sh
+++ key-value-app/start-db.sh
@@ -19,11 +19,25 @@ KEY_VALUE_PASSWORD="key-value-password"
 ROOT_USER="root-user"
 ROOT_PASSWORD="root-password"
 
+# Connectivity
+LOCALHOST_PORT=27017
+CONTAINER_PORT=27017
+
+NETWORK_NAME="keyvalue-net"
+
+# Storage
+VOLUME_NAME="keyvalue-data"
+VOLUME_CONTAINER_PATH="/data/db"
+
+# Start MongoDB container
 docker run --rm -d --name $DB_CONTAINER_NAME \
   -e MONGODB_INITDB_ROOT_USERNAME=$ROOT_USER \
   -e MONGODB_INITDB_ROOT_PASSWORD=$ROOT_PASSWORD \
   -e KEY_VALUE_DB=$KEY_VALUE_DB \
   -e KEY_VALUE_USER=$KEY_VALUE_USER \
   -e KEY_VALUE_PASSWORD=$KEY_VALUE_PASSWORD \
+  -p $LOCALHOST_PORT:$CONTAINER_PORT \
+  -v $VOLUME_NAME:$VOLUME_CONTAINER_PATH \
   -v ./db-config/mongo-init.js:/docker-entrypoint-initdb.d/mongo-init.js:ro \
+  --network $NETWORK_NAME \
   $MONGODB_IMAGE:$MONGODB_TAG
