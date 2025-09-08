87282181788f756add03e032f081bfa564a7f4d4
y2hhbmr1 <y2hhbmr1+glsl@bugcrowdninja.com>
2025-09-08 20:37:02 +0530

    89. Adding Credentials for the Key-Value Database

--- key-value-app/start-db.sh
+++ key-value-app/start-db.sh
@@ -7,13 +7,23 @@ MONGODB_TAG="7.0-ubuntu2204"
 # Container name
 CONTAINER_NAME="mongodb"
 
+
+
+# Key-value app credentials
+KEY_VALUE_DB="key-value-db"
+KEY_VALUE_USER="key-value-user"
+KEY_VALUE_PASSWORD="key-value-password"
+
+
 # Root credentials
 ROOT_USER="root-user"
 ROOT_PASSWORD="root-password"
 
-docker run -d \
-  --name $CONTAINER_NAME \
-  --rm \
-  -e MONGO_INITDB_ROOT_USERNAME=$ROOT_USER \
-  -e MONGO_INITDB_ROOT_PASSWORD=$ROOT_PASSWORD \
+docker run --rm -d --name $DB_CONTAINER_NAME \
+  -e MONGODB_INITDB_ROOT_USERNAME=$ROOT_USER \
+  -e MONGODB_INITDB_ROOT_PASSWORD=$ROOT_PASSWORD \
+  -e KEY_VALUE_DB=$KEY_VALUE_DB \
+  -e KEY_VALUE_USER=$KEY_VALUE_USER \
+  -e KEY_VALUE_PASSWORD=$KEY_VALUE_PASSWORD \
+  -v ./db-config/mongo-init.js:/docker-entrypoint-initdb.d/mongo-init.js:ro \
   $MONGODB_IMAGE:$MONGODB_TAG
