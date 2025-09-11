f64d435afe128be42aef4c932c505a7f5602bf04
y2hhbmr1 <y2hhbmr1+glsl@bugcrowdninja.com>
2025-09-11 23:31:59 +0530

    116. Setup Docker Compose for Notes Services

--- notes-rest-api/notes-backend/src/server.js
+++ notes-rest-api/notes-backend/src/server.js
@@ -1,8 +1,24 @@
-const express = require("express"); // import express
-const app = express(); // create app
+const express = require("express");
+const mongoose = require("mongoose");
+
+const app = express();
+
+const port = process.env.PORT;
 
 app.get("/", (req, res) => {
   res.json({ message: "Hello from notes" });
 });
 
-app.listen(3001, () => console.log("Notes server listening on port 3001"));
+mongoose
+  .connect(process.env.DB_URL)
+  .then(() => {
+    console.log("Connected to MongoDB! Starting server.");
+
+    app.listen(port, () => {
+      console.log(`Notes server listening on port ${port}`);
+    });
+  })
+  .catch((err) => {
+    console.error("Something went wrong!");
+    console.error(err);
+  });
