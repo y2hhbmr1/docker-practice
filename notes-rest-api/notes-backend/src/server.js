a7014ea0e84ac6e944fcaac843712c169a719ac6
y2hhbmr1 <y2hhbmr1+glsl@bugcrowdninja.com>
2025-09-12 02:33:23 +0530

    123 - 128

--- notes-rest-api/notes-backend/src/server.js
+++ notes-rest-api/notes-backend/src/server.js
@@ -1,24 +1,25 @@
-const express = require("express");
-const mongoose = require("mongoose");
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const { noteRouter } = require('./routes');
 
 const app = express();
 
-const port = process.env.PORT;
+app.use(bodyParser.json());
+app.use('/api/notes', noteRouter);
 
-app.get("/api/notes", (req, res) => {
-  res.json({ message: "Hello from notes" });
-});
+const port = process.env.PORT;
 
 mongoose
   .connect(process.env.DB_URL)
   .then(() => {
-    console.log("Connected to MongoDB! Starting server.");
+    console.log('Connected to MongoDB! Starting server.');
 
     app.listen(port, () => {
       console.log(`Notes server listening on port ${port}`);
     });
   })
   .catch((err) => {
-    console.error("Something went wrong!");
+    console.error('Something went wrong!');
     console.error(err);
   });
