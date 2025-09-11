e1ce2ed42c2b1cc62d382935d71685efca021d7d
y2hhbmr1 <y2hhbmr1+glsl@bugcrowdninja.com>
2025-09-11 22:19:39 +0530

    114. Dockerize the Notebooks Backend

--- notes-rest-api/notebooks-backend/src/server.js
+++ notes-rest-api/notebooks-backend/src/server.js
@@ -1,8 +1,12 @@
 const express = require("express"); // import express
 const app = express(); // create app
 
+const port = process.env.PORT;
+
 app.get("/", (req, res) => {
   res.json({ message: "Hello from notebooks" });
 });
 
-app.listen(3000, () => console.log("Notebooks server listening on port 3000"));
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
